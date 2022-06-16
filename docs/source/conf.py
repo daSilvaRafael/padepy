977cddc25d8052f1b10038c3fd556b466f38a101
Da Silva, Rafael <rafael6braga@gmail.com>
2022-06-16 15:27:18 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'padepy'
+copyright = '2022, Rafael'
+author = 'Rafael Pereira da Silva'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1'
 
 # -- General configuration
 
